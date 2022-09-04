1ce2c73c37b224bbbed8e929c842a7d0954ec438
Donald Duck <tang5722917@163.com>
2022-09-05 02:34:23 +0800

    Fix a error

--- Init/pkuxkx_init.lua
+++ Init/pkuxkx_init.lua
@@ -1,9 +1,18 @@
+--[[
+Author: Donald Duck tang5722917@163.com
+Date: 2022-08-25 20:21:21
+LastEditors: Donald Duck tang5722917@163.com
+LastEditTime: 2022-09-05 02:29:33
+FilePath: /mudlet_-auto-robot/Init/pkuxkx_init.lua
+Description:
+Copyright (c) 2022 by Donald Duck email: tang5722917@163.com, All Rights Reserved.
+--]]
 --
 --功能：脚本初始
 --
 --SysTrigger
 --初始化系统配置文件
-dofile(Lua_path.."init/pkuxkx_init_config.lua")
+dofile(Lua_path.."Init/pkuxkx_init_config.lua")
 --加载日志功能
 dofile(Lua_path.."Sys/pkuxkx_SysLog.lua")
 --加载核心功能
