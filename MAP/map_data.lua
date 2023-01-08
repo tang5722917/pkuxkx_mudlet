0bcba83eda8ceaf1cf90abc742252b5eab09a403
Tangzp <tang5722917@163.com>
2023-01-09 02:52:16 +0800

    添加Sys_table.merge 函数 将send 的时间间隔改为0.5s

--- MAP/map_data.lua
+++ MAP/map_data.lua
@@ -5,5 +5,9 @@ pkuxkx_map_data = pkuxkx_map_data or {}
 
 -- 世界地图
 pkuxkx_map_data.date = {}
+pkuxkx_map_table_1 = {'w','w','w','w','nw','w','w','w','w','w'}   --   扬州 - 信阳
+pkuxkx_map_table_2 = {'w','w','w','nw','w','w','w','w','w'}       --   信阳 - 襄阳
+pkuxkx_map_table_3 = {'w','w','w','w','w','w','w','w','w','w','w','w','w','s','sw','w','w','nw','w','w','w'}       --   襄阳 -- 武当山门
+pkuxkx_map_table_4 = {'wu','nu','nu','nu','w','nu','nu','eu','eu','nu','nu'}       --   武当山门 - 武当广场
 
 
--- Sys/pkuxkx_Sysinit.lua
+++ Sys/pkuxkx_Sysinit.lua
@@ -8,6 +8,8 @@ Description: Sys lua import
 Copyright (c) 2022 by Donald Duck email: tang5722917@163.com, All Rights Reserved.
 --]]
 --System 初始化导入
+dofile(Lua_path.."Sys/SysFun/pkuxkx_SysFun_table.lua")
+
 dofile(Lua_path.."Sys/pkuxkx_SysTrigger_Timer_Control.lua")
 dofile(Lua_path.."Sys/pkuxkx_SysCLKInit.lua")
 dofile(Lua_path.."Sys/pkuxkx_SysTrigger_Timer.lua")
